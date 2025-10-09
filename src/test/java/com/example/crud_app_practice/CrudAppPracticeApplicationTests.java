e48adb7af3d0299f54359655d5a04d1653d20868
Tamil Arasan <tamilarasan2910@gmail.com>
2025-10-09 08:58:37 +0530

    Initial commit

---
+++ src/test/java/com/example/crud_app_practice/CrudAppPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_app_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
