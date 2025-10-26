6f3c3577ca255de29064bb84628aa5202e29495a
Tamil Arasan <tamilarasan2910@gmail.com>
2025-10-27 00:35:18 +0530

    models are updated with new lombok annotations no args, all args constrctors, table, data, etc..

--- src/main/java/com/example/crud_app_practice/models/Category.java
+++ src/main/java/com/example/crud_app_practice/models/Category.java
@@ -1,19 +1,25 @@
 package com.example.crud_app_practice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
 public class Category extends BaseModel{
 
-  @NotBlank(message = "Name is required")
   @Size(min = 2, max = 50, message = "Name must be between 2 to 50 characters")
   private String name;
   
+  @NotBlank(message = "Description is required")
   private String description;
 }
--- src/main/java/com/example/crud_app_practice/models/Product.java
+++ src/main/java/com/example/crud_app_practice/models/Product.java
@@ -5,17 +5,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
 public class Product extends BaseModel {
   @NotBlank(message = "Title is required")
   @Size(min = 2, max = 50, message = "Title must be between 2 to 50 characters")
